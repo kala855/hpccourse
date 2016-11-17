54610fa57706985db3ff7f71e4cd1ede06eee1af
John Osorio <john@sirius.utp.edu.co>
2016-11-16 19:47:10 -0500

    Se realiza modificación en mpi_hello_world.sh para que se lance un proceso por nodo. Ideal para usar sólo la GPU disponible en cada sistema de Lovelace.

--- mpiexamples/mpi_hello_world/mpi_hello_world.sh
+++ mpiexamples/mpi_hello_world/mpi_hello_world.sh
@@ -2,7 +2,8 @@
 #
 #SBATCH --job-name=mpi_hello_world
 #SBATCH --output=res_mpi_hello_world.out
-#SBATCH --ntasks=48
+#SBATCH --ntasks=2
+#SBATCH --nodes=2
 #SBATCH --time=10:00
 #SBATCH --mem-per-cpu=100
 
