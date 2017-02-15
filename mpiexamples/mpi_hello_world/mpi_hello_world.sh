0b75ac26fa17788b1194cfea223ff1c03e99ead6
John Osorio <john@sirius.utp.edu.co>
2017-02-15 10:17:38 -0500

    Se adicionan ejemplos de openmp y slurm

--- mpiexamples/mpi_hello_world/mpi_hello_world.sh
+++ mpiexamples/mpi_hello_world/mpi_hello_world.sh
@@ -6,5 +6,6 @@
 #SBATCH --nodes=2
 #SBATCH --time=10:00
 #SBATCH --mem-per-cpu=100
+#SBATCH --gres=gpu:1
 
 mpirun mpi_hello_world
---
+++ openmexamples/omp_hello/omp_hello.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#SBATCH --job-name=omp_hello_world
+#SBATCH --output=res_omp_hello_world.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=10:00
+#SBATCH --mem-per-cpu=100
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+./omp_hello
